e249fb61306fdfdee84330ede55cbddd339aab2c
Kayeon Seon <gyseon20@duksung.ac.kr>
2021-12-02 18:52:17 +0900

    상품 목록 페이지에 대표 이미지 출력하기

--- mall/models.py
+++ mall/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Item(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
-
+    price = models.CharField(max_length=10, blank=True)
+    head_image = models.ImageField(upload_to='mall/images/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
