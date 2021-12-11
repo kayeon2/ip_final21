9d5a74d02e2e0cae46a3c3647a4cfcdff6a86543
Kayeon Seon <gyseon20@duksung.ac.kr>
2021-12-12 01:34:18 +0900

    요약문 추가하기

--- mall/models.py
+++ mall/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 class Item(models.Model):
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
-    price = models.CharField(max_length=10, blank=True)
-    head_image = models.ImageField(upload_to='mall/images/%Y/%m/%d/', blank=True)
+    price = models.CharField(max_length=10)
+    head_image = models.ImageField(upload_to='mall/images/%Y/%m/%d/')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
