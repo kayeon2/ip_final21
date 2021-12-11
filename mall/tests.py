a761aec3d89515bef17bcd33c46a65c4797fa8de
Kayeon Seon <gyseon20@duksung.ac.kr>
2021-12-12 03:20:11 +0900

    내비게이션 바와 푸터 모듈화하기

--- mall/tests.py
+++ mall/tests.py
@@ -6,19 +6,33 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Shop', navbar.text)
+        self.assertIn('About Us', navbar.text)
+
+        logo_btn = navbar.find('a', text='Navbar')
+        self.assertEqual(logo_btn.attrs['href'], '/')
+
+        home_btn = navbar.find('a', text='Home')
+        # self.assertEqual(home_btn.attrs['href'], '/')
+
+        mall_btn = navbar.find('a', text='Shop')
+        self.assertEqual(mall_btn.attrs['href'], '/mall/')
+
+        about_me_btn = navbar.find('a', text='About Us')
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
+
     def test_item_list(self):
         response = self.client.get('/mall/')
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
         self.assertEqual(soup.title.text, 'Mall')
-        navbar = soup.nav
-        self.assertIn('Shop', navbar.text)
-        self.assertIn('About Us', navbar.text)
         self.assertEqual(Item.objects.count(), 0)
 
         main_area = soup.find('div', id='main-area')
         self.assertIn('아직 상품이 없습니다.', main_area.text)
-
+        self.navbar_test(soup)
         #item_001 = Item.objects.create(
         #    title='첫 번째 상품입니다',
         #    content='Hello World!',
@@ -36,3 +50,4 @@ class TestView(TestCase):
         #self.assertIn(item_001.title, main_area.text)
         #self.assertIn(item_002.title, main_area.text)
         #self.assertNotIn('아직 상품이 없습니다.', main_area.text)
+
