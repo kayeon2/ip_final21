c93db871e89d9c020fb5c5d14fe014960fd93ebd
Kayeon Seon <gyseon20@duksung.ac.kr>
2021-12-14 03:45:51 +0900

    brand 필드 추가하기

--- mall/models.py
+++ mall/models.py
@@ -1,16 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+import os
 
 class Item(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
     content = models.TextField()
     price = models.CharField(max_length=10)
+
     head_image = models.ImageField(upload_to='mall/images/%Y/%m/%d/')
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    brand = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.brand}'
 
     def get_absolute_url(self):
         return f'/mall/{self.pk}/'
