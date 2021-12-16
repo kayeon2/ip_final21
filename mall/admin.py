f8aa15f6a451a71a9885e82af72b0154203aef4f
Kayeon Seon <gyseon20@duksung.ac.kr>
2021-12-16 19:11:26 +0900

    상품 목록 페이지와 상세 페이지에 태그 기능 추가하기

--- mall/admin.py
+++ mall/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Item, Category
+from .models import Item, Category, Tag
 
 admin.site.register(Item)
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 
+class TagAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Tag, TagAdmin)
