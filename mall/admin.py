5318f08e76f42e3c5c99abc17e79680c1008d242
Kayeon Seon <gyseon20@duksung.ac.kr>
2021-12-16 18:42:09 +0900

    아이템 목록, 상세 페이지에 카테고리 기능 추가하기

--- mall/admin.py
+++ mall/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Item
+from .models import Item, Category
 
 admin.site.register(Item)
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(Category, CategoryAdmin)
--- mall/views.py
+++ mall/views.py
@@ -1,11 +1,24 @@
+from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Item
+from .models import Item, Category
 
 class ItemList(ListView):
     model = Item
     ordering = '-pk'
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ItemList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_item_count'] = Item.objects.filter(category=None).count()
+        return context
+
 class ItemDetail(DetailView):
     model = Item
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ItemDetail, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_item_count'] = Item.objects.filter(category=None).count()
+        return context
 
 # Create your views here.
