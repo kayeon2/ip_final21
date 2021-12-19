0c35bfc95719b1fb1c2249f29cb56d764878165c
Kayeon Seon <gyseon20@duksung.ac.kr>
2021-12-20 02:00:48 +0900

    My Page 수정까지

--- single_pages/admin.py
+++ single_pages/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from mall.models import Comment
 
 # Register your models here.
--- single_pages/views.py
+++ single_pages/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from mall.models import Item
+from mall.models import Item, Comment
 
 def landing(request):
     recent_items = Item.objects.order_by('-pk')[:3]
@@ -18,7 +18,9 @@ def about_me(request):
     )
 
 def my_page(request):
+    comments = Comment.objects.filter(author=request.user)
     return render(
         request,
-        'single_pages/my_page.html'
+        'single_pages/my_page.html',
+        context={'comments': comments}
     )
